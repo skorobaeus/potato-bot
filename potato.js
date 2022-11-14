c148a60cad7c15b169fdb40f0b574b72b43678ee
Svetlana Korobtseva <wizzzjer@gmail.com>
2022-11-15 02:40:31 +0600

    disables auth.json

--- potato.js
+++ potato.js
@@ -1,4 +1,4 @@
-const config = require('./auth.json');
+// const config = require('./auth.json');
 
 const { REST, Routes } = require('discord.js');
 // const rest = new REST({ version: '10' }).setToken(config.token);
